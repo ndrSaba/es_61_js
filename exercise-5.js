19bb25f1c5c1173b607fae82285950181726f917
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-06-14 15:20:16 +0200

    es fatto

---
+++ exercise-5.js
@@ -0,0 +1,74 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const person = persons.find(item => item.id === id);
+      
+      if (person) {
+        return resolve(JSON.stringify(person));
+      }
+
+      return reject(`Person with id: ${id} doesn't exist`);
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const job = jobs.find(item => item.id === id);
+      
+      if (job) {
+        return resolve(job);
+      }
+
+      return reject(`Person with job title id ${id} doesn't exist`);
+    }, 500);
+  });
+}
+
+
+let personId = fetchPersonById(3);
+let personJob = fetchJobById(3);
+Promise.race([personId, personJob])
+.then((id) => {
+  console.log(id)
+})
+.catch((err) => {
+  console.log(`è stato provocato un errore:`, err)
+})
